514d02046c2c385fcfade5cc0dc7487ef902f348
adekang <201497709@qq.com>
2021-06-12 15:04:24 +0800

    实现button的基本布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './styles/index.scss';
+import Button, {ButtonSize, ButtonType} from './components/Button/button';
 
 function App() {
   return (
     <div>
-      123
+      <Button>Hello</Button>
+      <Button disabled>Hello</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Hello</Button>
+      <Button btnType={ButtonType.Link} href={'www.baidu.com'}>Hello</Button>
     </div>
   );
 }
