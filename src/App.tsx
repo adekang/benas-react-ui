b6e209faf89f8a01900c9e47dde4efef9d207128
adekang <201497709@qq.com>
2021-06-14 16:37:10 +0800

    实现 Input 组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,19 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './styles/index.scss';
 import ButtonDemo from './components/Button/ButtonDemo';
 import MenuDemo from './components/Menu/MenuDemo';
 import IconDemo from './components/Icon/IconDemo';
-
-import Button from './components/Button/button';
-import Transition from './components/Transition/transition';
+import TransitionDemo from './components/Transition/TransitionDemo';
+import InputDemo from './components/Input/InputDemo';
 
 function App() {
-  const [show, setShow] = useState(false);
   return (
     <div>
       <ButtonDemo/>
       <MenuDemo/>
       <IconDemo/>
-      <Button size="lg" onClick={() => {setShow(!show);}}>toggle</Button>
-      <Transition in={show} timeout={250} animation={'zoom-in-top'} wrapper={true}>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-      </Transition>
+      <TransitionDemo/>
+      <InputDemo/>
     </div>
   );
 }
